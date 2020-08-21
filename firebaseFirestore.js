c4f7204fbebc4fb478d417e2c5574c0055a405e7
cristi ene <cristi@bocris.ro>
2020-08-21 17:52:00 +0300

    first commit

---
+++ firebaseFirestore.js
@@ -0,0 +1,56 @@
+const list = document.querySelector('ul');
+const form = document.querySelector('form');
+
+const addRecipe = (recipe, id)=>{
+    let time = recipe.created_at.toDate();
+    let html = `
+        <li data-id="${id}">
+            <div>${recipe.title}</div>
+            <div>${time}</div>
+            <button class="btn btn-danger btn-sm my-2">delete</button>
+        </li>
+    `;
+
+    list.innerHTML += html;
+}
+
+// get documents
+db.collection('recipes').get().then((snapshot)=>{
+    //when we have the data
+    snapshot.docs.forEach(doc => {
+        addRecipe(doc.data(), doc.id);
+    })
+}).catch(err=> {
+    console.log(err);
+});
+
+// add documents
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    const now = new Date();
+
+    const recipe = {
+        title:form.recipe.value,
+        created_at:firebase.firestore.Timestamp.fromDate(now),  
+    };
+
+    db.collection('recipes').add(recipe).then(() =>{
+        console.log('recipe added');
+    }).catch((err) =>{
+        console.log(err);
+    });
+});
+
+
+// deleting data
+list.addEventListener('click', event =>{
+    //console.log(event);
+    if(event.target.tagName === 'BUTTON'){
+        const id = event.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete().then(() => {
+            console.log('recipe deleted');
+        });
+    }
+})
